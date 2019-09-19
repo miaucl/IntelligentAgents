27e1c9000b206d9996d4d2a200d5302d5e5d77ea
miaucl <cy.li@gmx.ch>
2019-09-19 16:43:21 +0200

    Added the limit of grass to 0-maxGrass and adapted the algorithm to be efficient - Added the visualization of the grass grid - Commented all the functions with JSDoc

--- vianey-lippuner-in/src/RabbitsGrassSimulationModel.java
+++ vianey-lippuner-in/src/RabbitsGrassSimulationModel.java
@@ -1,7 +1,10 @@
+import java.awt.Color;
 import uchicago.src.sim.engine.Schedule;
 import uchicago.src.sim.engine.SimModelImpl;
 import uchicago.src.sim.engine.SimInit;
 import uchicago.src.sim.gui.DisplaySurface;
+import uchicago.src.sim.gui.ColorMap;
+import uchicago.src.sim.gui.Value2DDisplay;
 
 /**
  * Class that implements the simulation model for the rabbits grass
@@ -9,7 +12,7 @@ import uchicago.src.sim.gui.DisplaySurface;
  * order to run Repast simulation. It manages the entire RePast
  * environment and the simulation.
  *
- * @author Vianey, Lippuner
+ * @author A. Vianey, C. Lippuner
  */
 
 
@@ -48,55 +51,10 @@ public class RabbitsGrassSimulationModel extends SimModelImpl
 			init.loadModel(model, args[0], Boolean.parseBoolean(args[1]));
 		
 	}
-
-	@Override
-	public void begin()
-	{
-	    buildModel();
-	    buildSchedule();
-	    buildDisplay();
-	}
-
-	public void buildModel()
-	{
-		System.out.println("Build model");
-	    grassSpace = new RabbitsGrassSimulationSpace(gridSize, gridSize);
-	    grassSpace.spreadGrass(numInitGrass);
-
-	}
 	
-	public void buildSchedule()
-	{
-		System.out.println("Build schedule");
-	}
-	
-	public void buildDisplay()
-	{
-		System.out.println("Build display");
-	}
-
-	@Override
-	public String[] getInitParam() 
-	{
-		// TODO Auto-generated method stub
-		// Parameters to be set by users via the Repast UI slider bar
-		// Do "not" modify the parameters names provided in the skeleton code, you can add more if you want 
-		String[] params = { "GridSize", "NumInitRabbits", "NumInitGrass", "GrassGrowthRate", "BirthThreshold" };
-		return params;
-	}
-
-	@Override
-	public String getName() 
-	{
-		return "Rabbit Grass Simulation";
-	}
-
-	@Override
-	public Schedule getSchedule() 
-	{
-		return schedule;
-	}
-
+	/**
+	 * The setup routine to clean all remaining objects and left-overs
+	 */	
 	@Override
 	public void setup() 
 	{			
@@ -114,51 +72,177 @@ public class RabbitsGrassSimulationModel extends SimModelImpl
 	    registerDisplaySurface("Rabbits Grass Model Window 1", displaySurf);
 	}
 
+
+	/**
+	 * Begin the simulation and setup all objects and configurations
+	 */	
+	@Override
+	public void begin()
+	{
+	    buildModel();
+	    buildSchedule();
+	    buildDisplay();
+	    
+	    displaySurf.display();
+	}
+
+	/**
+	 * Build the model
+	 * Add the simulation space
+	 */	
+	private void buildModel()
+	{
+		System.out.println("Build model");
+	    grassSpace = new RabbitsGrassSimulationSpace(gridSize, gridSize);
+	    grassSpace.spreadGrass(numInitGrass);
+
+	}
+	
+	/**
+	 * Build the schedule
+	 */	
+	private void buildSchedule()
+	{
+		System.out.println("Build schedule");
+	}
+	
+	/**
+	 * Build the display
+	 * Create a window to display the space
+	 */	
+	private void buildDisplay()
+	{
+		System.out.println("Build display");
+		
+		ColorMap map = new ColorMap();
+
+	    map.mapColor(0, Color.white);
+	    map.mapColor(1, Color.green);
+
+	    Value2DDisplay displayGrass = new Value2DDisplay(grassSpace.getCurrentGrassSpace(), map);
+
+	    displaySurf.addDisplayable(displayGrass, "Grass");
+	}
+
+	/**
+	 * Get all available parameters of the model
+	 * @return A string list of the parameters
+	 */	
+	@Override
+	public String[] getInitParam() 
+	{
+		// Parameters to be set by users via the Repast UI slider bar
+		// Do "not" modify the parameters names provided in the skeleton code, you can add more if you want 
+		String[] params = { "GridSize", "NumInitRabbits", "NumInitGrass", "GrassGrowthRate", "BirthThreshold" };
+		return params;
+	}
+
+	/**
+	 * Get the name of the simulation model
+	 * @return The model name
+	 */	
+	@Override
+	public String getName() 
+	{
+		return "Rabbit Grass Simulation";
+	}
+
+	/**
+	 * Get the schedule of the simulation
+	 * @return A schedule object
+	 */	
+	@Override
+	public Schedule getSchedule() 
+	{
+		return schedule;
+	}
+
+
+	/**
+	 * Getter for 'gridSize'
+	 * @return The size of the space
+	 */	
 	public int getGridSize() 
 	{
 		return gridSize;
 	}
 
+	/**
+	 * Setter for 'gridSize'
+	 * @param gridSize
+	 */	
 	public void setGridSize(int gridSize) 
 	{
 		this.gridSize = gridSize;
 	}
 
+	/**
+	 * Getter for 'numInitRabbits'
+	 * @return Initial rabbits placed in the space
+	 */	
 	public int getNumInitRabbits() 
 	{
 		return numInitRabbits;
 	}
 
+	/**
+	 * Setter for 'numInitRabbits'
+	 * @param numInitRabbits
+	 */	
 	public void setNumInitRabbits(int numInitRabbits) 
 	{
 		this.numInitRabbits = numInitRabbits;
 	}
 
+	/**
+	 * Getter for 'numInitGrass'
+	 * @return Initial grass placed in the space
+	 */	
 	public int getNumInitGrass() 
 	{
 		return numInitGrass;
 	}
 
+	/**
+	 * Setter for 'numInitGrass'
+	 * @param numInitGrass
+	 */	
 	public void setNumInitGrass(int numInitGrass) 
 	{
 		this.numInitGrass = numInitGrass;
 	}
 
+	/**
+	 * Getter for 'grassGrowthRate'
+	 * @return Rate the grass should grow
+	 */	
 	public double getGrassGrowthRate() 
 	{
 		return grassGrowthRate;
 	}
 
+	/**
+	 * Setter for 'grassGrowthRate'
+	 * @param grassGrowthRate
+	 */	
 	public void setGrassGrowthRate(double grassGrowthRate) 
 	{
 		this.grassGrowthRate = grassGrowthRate;
 	}
 
+	/**
+	 * Getter for 'birthThreshold'
+	 * @return Grass threshold when reproduction should happen
+	 */	
 	public int getBirthThreshold() 
 	{
 		return birthThreshold;
 	}
 
+	/**
+	 * Setter for 'birthThreshold'
+	 * @param birthThreshold
+	 */	
 	public void setBirthThreshold(int birthThreshold) 
 	{
 		this.birthThreshold = birthThreshold;
--- vianey-lippuner-in/src/RabbitsGrassSimulationSpace.java
+++ vianey-lippuner-in/src/RabbitsGrassSimulationSpace.java
@@ -9,8 +9,14 @@ public class RabbitsGrassSimulationSpace
 {
 	private Object2DGrid grassSpace;
 
+	/**
+	 * Constructor of the Simulation Space with a fixed size
+	 * @param xSize
+	 * @param ySize
+	 */	
 	public RabbitsGrassSimulationSpace(int xSize, int ySize)
 	{
+		// Create a space to store where is grass (init with 0=no-grass)
 		grassSpace = new Object2DGrid(xSize, ySize);
 	    for(int i = 0; i < xSize; i++)
 	    {
@@ -27,24 +33,69 @@ public class RabbitsGrassSimulationSpace
 	 */	
 	public void spreadGrass(int grass)
 	{	
-	    // Randomly place grass in grassSpace
-	    for(int i = 0; i < grass; i++)
-	    {
-	    	// TODO CARE FOR TOO MUCH GRASS
-	    	
-			// Choose coordinates
-	    	int x, y;
-	    	do
-	    	{
-	    		x = (int)(Math.random()*(grassSpace.getSizeX()));
-			 	y = (int)(Math.random()*(grassSpace.getSizeY()));
-	    	} while (((Integer)grassSpace.getObjectAt(x,y)).intValue() > 0);
+		int maxGrass = grassSpace.getSizeX() * grassSpace.getSizeY();
+    	// CARE FOR TOO MUCH GRASS
+    	if (grass > maxGrass)
+    	{
+    		System.out.println("Capped the grass at max cell " + maxGrass + "!");
+    		grass = maxGrass;
+    	}    	
+    	
+    	// CARE FOR NOT ENOUGH GRASS
+    	if (grass < 0)
+    	{
+    		System.out.println("Capped the grass at min cell 0!");
+    		grass = 0;
+    	}
+    	
+    	// More grass than no-grass
+    	if (grass > maxGrass / 2)
+    	{
+    		// Overwrite the space to store where is grass (init with 1=grass)
+    		for(int i = 0; i < grassSpace.getSizeX(); i++)
+    	    {
+    	    	for(int j = 0; j < grassSpace.getSizeY(); j++)
+    	    	{
+    	    		grassSpace.putObjectAt(i,j,new Integer(1));
+    	    	}
+    	    }
+    		
+    	    // Randomly remove grass in grassSpace
+    	    for (int i = 0; i < maxGrass - grass; i++)
+    	    {	    	
+    			// Choose coordinates (verify that there is no double assignment)
+    	    	int x, y;
+    	    	do
+    	    	{
+    	    		x = (int)(Math.random()*(grassSpace.getSizeX()));
+    			 	y = (int)(Math.random()*(grassSpace.getSizeY()));
+    	    	} while (((Integer)grassSpace.getObjectAt(x,y)).intValue() < 1);
 
-			
-	    	// Set to 1=grass
-	    	grassSpace.putObjectAt(x, y, new Integer(1));
-			
-	    }
+    			
+    	    	// Set to 0=no-grass
+    	    	grassSpace.putObjectAt(x, y, new Integer(0));
+    	    }    		
+
+    	}
+    	else
+    	{
+    	    // Randomly place grass in grassSpace
+    	    for (int i = 0; i < grass; i++)
+    	    {	    	
+    			// Choose coordinates (verify that there is no double assignment)
+    	    	int x, y;
+    	    	do
+    	    	{
+    	    		x = (int)(Math.random()*(grassSpace.getSizeX()));
+    			 	y = (int)(Math.random()*(grassSpace.getSizeY()));
+    	    	} while (((Integer)grassSpace.getObjectAt(x,y)).intValue() > 0);
+
+    			
+    	    	// Set to 1=grass
+    	    	grassSpace.putObjectAt(x, y, new Integer(1));
+    			
+    	    }    		
+    	}
 	}
 	
 	/**
@@ -56,5 +107,14 @@ public class RabbitsGrassSimulationSpace
 	public boolean hasGrassAt(int x, int y)
 	{
 		return ((Integer)grassSpace.getObjectAt(x,y)).intValue() == 1;
+	}
+	
+	/**
+	 * Get the current grass space
+	 * @return The current grass space
+	 */
+	public Object2DGrid getCurrentGrassSpace()
+	{
+	    return grassSpace;
 	}
 }
