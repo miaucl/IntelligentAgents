495f7b32380d9cd199eaf051cc43eed72673f4d5
AntonainV <antonin.vianey@live.com>
2019-09-30 15:07:23 +0200

    ADD: 	-begin setup of R

--- reactive/src/template/ReactiveTemplate.java
+++ reactive/src/template/ReactiveTemplate.java
@@ -20,6 +20,9 @@ public class ReactiveTemplate implements ReactiveBehavior {
 	private int numActions;
 	private Agent myAgent;
 
+	private double[][] R;
+	private double [][][] T;
+
 	@Override
 	public void setup(Topology topology, TaskDistribution td, Agent agent) {
 
@@ -31,7 +34,34 @@ public class ReactiveTemplate implements ReactiveBehavior {
 		this.random = new Random();
 		this.pPickup = discount;
 		this.numActions = 0;
-		this.myAgent = agent;
+		this.myAgent = agent;	
+		
+		int sizeCities = topology.size();
+		R = new double[sizeCities*2][sizeCities+1];
+		T = new double[sizeCities*2][sizeCities+1][sizeCities*2];
+		
+		for (int i = 0; i < R.length; i++)
+		{
+			int index = i%sizeCities;
+			boolean availablePacket = i>=sizeCities;
+			City source = topology.cities().get(index);
+			
+			for (int j = 0; j < R[i].length; j++)
+			{
+				City dest = topology.cities().get(j+1); //index 0 is pickAndDeliver action
+				
+				R[i][j]= Double.NEGATIVE_INFINITY;
+				
+				if (availablePacket && j==0) //if packet available and pickAndDeliver action
+				{
+					R[i][j]=td.reward(source, dest);
+				}
+				else if(source.hasNeighbor(dest))
+				{
+					R[i][j]=0;	//-1?
+				}
+			} 
+		} 
 	}
 
 	@Override
